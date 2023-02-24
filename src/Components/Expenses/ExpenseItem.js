ce629f4f24a39b42f670bf52ef96575d08e2c10d
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-02-24 19:39:45 +0530

    adding buttons and working on events.

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -2,8 +2,16 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import { createElement } from "react";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('clicked!!!!')
+  }
+  const deleteHandler  = () => {
+    console.log('Deleted!!!')
+  } 
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +20,8 @@ const ExpenseItem = (props) => {
         location={props.location}
         amount={props.amount}
       />
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete</button>
     </Card>
   );
 }
