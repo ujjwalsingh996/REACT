c7448d0ce7436d05302a80c396ef8fee2b381ade
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-02-24 15:56:09 +0530

    Using fat arrow functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -32,35 +32,13 @@ function App() {
     },
   ];
 
-  return (
+  return(
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-       title={expenses[0].title} 
-       amount={expenses[0].amount} 
-       date={expenses[0].date}
-       location={expenses[0].location}
-       ></ExpenseItem>
-      <ExpenseItem
-       title={expenses[1].title} 
-       amount={expenses[1].amount} 
-       date={expenses[1].date}
-       location={expenses[1].location}
-       ></ExpenseItem>
-       <ExpenseItem
-       title={expenses[2].title} 
-       amount={expenses[2].amount} 
-       date={expenses[2].date}
-       location={expenses[2].location}
-       ></ExpenseItem>
-       <ExpenseItem
-       title={expenses[3].title} 
-       amount={expenses[3].amount} 
-       date={expenses[3].date}
-       location={expenses[3].location}
-       ></ExpenseItem>
+      <h2>Lets Get Started!</h2>
+      <Expenses items={expenses}/>
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        location={props.location}
+        amount={props.amount}
+      />
+    </Card>
+  );
+}
+
+export default ExpenseItem;
