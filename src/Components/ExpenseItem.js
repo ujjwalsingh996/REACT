7fd0a8ee6487f78e954d5840a2415a38eb3d5613
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-02-24 12:22:57 +0530

    Hard coding expenses

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem() {
+    return (
+        <div> 
+            <h2> Expense Items </h2>
+            <h4>Food Rs10</h4>
+            <h4>Petrol Rs100</h4>
+            <h4>Movies Rs200
+                
+            </h4>
+        </div>
+   
+
+    )
+}
+
+export default ExpenseItem;
