1e50d0b33e4fd2892c021260715430384670f4fd
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-02-27 19:41:20 +0530

    ExpensesList addition

---
+++ src/Components/Expenses/ExpensesList.js
@@ -0,0 +1,39 @@
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
+  }
+  if(props.items.length === 1)
+  {
+    return (
+        <ul className="expenses-list">
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      
+      <h2>Only Single Expense here. Please Add more</h2>
+      </ul>
+    )
+  }
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
